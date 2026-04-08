e5b9778b0b782bd7e92fc2474028bfd436c3c4b6
chennating <natingchen@outlook.com>
2026-04-08 19:44:06 +0200

    disorder submit

---
+++ disorder.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   disorder.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chennating <chennating@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 19:43:26 by chennating        #+#    #+#             */
+/*   Updated: 2026/04/08 19:43:29 by chennating       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+double	compute_disorder(t_list *a)
+{
+	double	mistakes;
+	double	total_pairs;
+	t_list	*current;
+	t_list	*next;
+
+	mistakes = 0;
+	total_pairs = 0;
+	current = a;
+	next = a->next;
+	while (current)
+	{
+		next = current->next;
+		while (next)
+		{
+			if (current->content > next->content)
+				mistakes ++;
+			next = next->next;
+			total_pairs ++;
+		}
+		current = current->next;
+	}
+	if (total_pairs == 0)
+		return (0);
+	return (mistakes / total_pairs);
+}
+
+// static	t_list *build_list(int *value, int num)
+// {
+// 	t_list *list;
+// 	int	i = 0;
+
+// 	list = NULL;
+// 	while (i < num)
+// 	{
+// 		ft_lstadd_back(&list, ft_lstnew(value[i]));
+// 		i ++;
+// 	}
+// 	return (list);
+// }
+
+// static void print_list(t_list *list)
+// {
+// 	while (list)
+// 	{
+// 		printf("%d\n", list->content);
+// 		if (list->next)
+// 			printf("->");
+// 		list = list->next;
+// 	}
+// }
+
+// int	main()
+// {
+// 	int		values[] = {5, 4, 3, 1};
+// 	t_list	*a;
+
+// 	a = build_list(values, 4);
+// 	print_list(a);
+// 	printf("disorder = %.3f\n", compute_disorder(a));
+// 	return (0);
+// }
