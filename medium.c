d6e0754b38a4507e1e853eb8b0fcd49dbc7d4e3f
Francisco Luthra <fluthra@f0r1s12.codam.nl>
2026-04-13 14:40:43 +0200

    push

--- medium.c
+++ medium.c
@@ -26,13 +26,14 @@ void	push_chunk(t_data *data, int chunk_index, int stack)
 	L2 = data->numbers[data->chunks[chunk_index][LOW]];
 	H2 = data->numbers[data->chunks[chunk_index][HIGH]];
 	pushed = 0;
-	printf("lstsize: %d\n", ft_lstsize(data->stacks[A]));
-	printf("chunk 0 %d   %d\n", data->chunks[0][LOW], data->chunks[0][HIGH]);
-	printf("chunk 1 %d   %d\n", data->chunks[1][LOW], data->chunks[1][HIGH]);
-	printf("c_size %d", data->chunks[chunk_index][HIGH] - data->chunks[chunk_index][LOW] + 1);
-	print_chunks(data);
-	while (pushed < data->chunks[chunk_index][LOW] - data->chunks[chunk_index][LOW] + 1)
+	// printf("lstsize: %d\n", ft_lstsize(data->stacks[A]));
+	// printf("chunk 0 %d   %d\n", data->chunks[0][LOW], data->chunks[0][HIGH]);
+	// printf("chunk 1 %d   %d\n", data->chunks[1][LOW], data->chunks[1][HIGH]);
+	// printf("c_size %d", data->chunks[chunk_index][HIGH] - data->chunks[chunk_index][LOW] + 1);
+	// print_chunks(data);
+	while (pushed < data->chunks[chunk_index][HIGH] - data->chunks[chunk_index][LOW] + 1)
 	{
+		// printf("c_size %d\n", chunksize(data->chunks, chunk_index));
 		if ((data->stacks[stack]->content >= L2 && data->stacks[stack]->content <= H2))
 		{
 			push_to(data, 1 - stack);
@@ -48,15 +49,33 @@ void	four_chunks(t_data *data)
 	data->numbers = indexate(data->stacks[A]);
 	data->chunks[0][LOW] = 0;	//FIRST CHUNK
 	data->chunks[0][HIGH] = ft_lstsize(data->stacks[A]) - 1;	//FIRST CHUNK
-	printf("%d   %d\n", data->chunks[0][LOW], data->chunks[0][HIGH]);
+	data->chunk_count = 1;
+	// printf("%d   %d\n", data->chunks[0][LOW], data->chunks[0][HIGH]);
 	div_chunk(data->chunks, 0, data);	//DIVIDE the first chunk into two
 	//print_stacks(data->stacks[A], data->stacks[B]);
 	push_chunk(data, 1, A);	//PUSH the second chunk to B
-	//print_stacks(data->stacks[A], data->stacks[B]);
+	print_stacks(data->stacks[A], data->stacks[B]);
 	//DIVIDE CHUNK 1, AND PUSH CHUNK 2 A
-	//PUSH CHUNK 2 back to B
-	//DIVIDE CHUNK 0, AND PUSH CHUNK 3 to B
-	//PUSH CHUNK 0 to B
-
+	div_chunk(data->chunks, 1, data);
+	push_chunk(data, 2, B);
+	print_stacks(data->stacks[A], data->stacks[B]);
+	// //PUSH CHUNK 2 back to B
+	push_chunk(data, 2, A);
+	print_stacks(data->stacks[A], data->stacks[B]);
+	// //DIVIDE CHUNK 0, AND PUSH CHUNK 3 to B
+	div_chunk(data->chunks, 0, data);
+	push_chunk(data, 3, A);
+	print_stacks(data->stacks[A], data->stacks[B]);
+	// //PUSH CHUNK 0 to B
+	push_chunk(data, 0, A);
+	// print_chunks(data);
+	print_stacks(data->stacks[A], data->stacks[B]);
 	//Per Chunk push the largest number first to A.
+	int		chunk_order[4] = {0, 3, 1, 2};
+	// h = 0, l = 1
+	// ll = 2, hl = 1
+	// lh = 3 
+	push_largest(data, chunk_order);
+	print_stacks(data->stacks[A], data->stacks[B]);
+	printf("Sorted %d numbers in %d operations\n %d ops per number\n", ft_lstsize(data->stacks[A]), data->count_ops, data->count_ops / ft_lstsize(data->stacks[A]));
 }
