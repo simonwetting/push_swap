692bfd84f0d74afd9afbb83f0ef89e632ed0af15
Francisco Luthra <fluthra@f0r2s1.codam.nl>
2026-04-20 13:17:08 +0200

    push

--- bench_arg.c
+++ bench_arg.c
@@ -1,6 +1,21 @@
-int		bench_arg(char **args)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   benchmark.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swetting <swetting@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/04/14 14:21:10 by swetting      #+#    #+#                 */
+/*   Updated: 2026/04/15 13:43:01 by swetting      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	bench_arg(char **args)
 {
-	if (ft_strncmp(args[1], "--bench", 11) == 0 || ft_strncmp(args[2], "--bench", 11) == 0)
+	if (ft_strncmp(args[1], "--bench", 11) == 0
+		|| ft_strncmp(args[2], "--bench", 11) == 0)
 		return (1);
 	return (0);
 }
